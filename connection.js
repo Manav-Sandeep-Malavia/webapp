6de13b14bac9c633008f0d8c7a987ce064632dcd
manavmalavia18 <114517641+manavmalavia18@users.noreply.github.com>
2024-03-25 18:24:25 -0400

    new commit

--- connection.js
+++ connection.js
@@ -18,7 +18,7 @@ const logger = winston.createLogger({
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     logging: false,
-    dialect: process.env.DIALECT,
+    dialect: 'mysql',
     pool: {
         max: 10, 
         min: 0, 
